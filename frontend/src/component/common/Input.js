e385140ea942cff0a0775d6f2719c92d9e22d62f
yeji0120 <yaeji120@gmail.com>
2020-03-27 17:39:40 +0900

    [Add Input Compnent]
    - input component 생성
    - login page에 적용 후 레이아웃 1차 수정

--- frontend/src/component/common/Input.js
+++ frontend/src/component/common/Input.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
-const Input = () => {
-  return <div></div>;
+const Input = props => {
+  const [innerText, setInnerText] = useState("");
+  console.log(innerText);
+
+  return (
+    <Container>
+      <InputBox
+        {...props}
+        type="text"
+        placeholder={props.placeholder}
+        onChange={e => console.log(e.target.value)}
+      />
+    </Container>
+  );
 };
 
 export default Input;
+
+const Container = styled.div``;
+
+const InputBox = styled.input`
+  width: ${props => props.width}px;
+  height: ${props => props.height}px;
+  margin-bottom: 15px;
+  padding: 20px;
+  border: 1px solid #e5e5e5;
+  border-radius: 4px;
+  color: #333333;
+  font-size: 14px;
+  &:focus {
+    border: 1px solid #333333;
+    color: #333333;
+  }
+`;
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,10 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import Input from "src/component/common/Input";
 
 const Login = () => {
   return (
     <Container>
-      <BGImage />
+      <BgBox>
+        <BgImage />
+      </BgBox>
+      <LoginBox>
+        <InputBox>
+          <Input width="300" height="34" placeholder="셀러 아이디" />
+          <Input width="300" height="34" placeholder="셀러 비밀번호" />
+        </InputBox>
+      </LoginBox>
     </Container>
   );
 };
@@ -12,14 +21,32 @@ const Login = () => {
 export default Login;
 
 const Container = styled.div`
+  border: 2px dotted pink;
+`;
+
+const BgBox = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 60px;
 `;
 
-const BGImage = styled.div`
+const BgImage = styled.div`
+  width: 130px;
+  height: 52px;
+  margin-bottom: 15px;
   background-image: url("http://sadmin.brandi.co.kr/include/img/logo_seller_admin_1.png");
-  width: 300px;
-  height: 150px;
+  background-size: cover;
   background-repeat: no-repeat;
+`;
+
+const LoginBox = styled.div`
+  width: 360px;
+  margin: 0px auto;
+  border: 1px dotted skyblue;
+`;
+
+const InputBox = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 `;
