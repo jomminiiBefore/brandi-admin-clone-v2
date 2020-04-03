a69e7e6b59e8e572771a95e6b5f1f99fb40de755
valentin1235 <valentin123@naver.com>
2020-04-03 21:20:00 +0900

    [셀러 리스트 표출 API] - 데코레이터에서 넘어온 유저정보를 확인하여 마스터 권한이면 셀러 리스트를 표출 - sql 파일 수정

--- backend/image/view/image_view.py
+++ backend/image/view/image_view.py
@@ -5,14 +5,20 @@ from image.service.image_service import ImageService
 class ImageView:
     image_app = Blueprint('image_app', __name__, url_prefix='/image')
 
-    @image_app.route('/resize', methods=['POST'])
-    def upload_resized_image():
+    @image_app.route('/product', methods=['POST'])
+    def upload_product_image():
         image_service = ImageService()
-        image_upload_result = image_service.upload_resized_image(request)
+        image_upload_result = image_service.upload_product_image(request)
         return image_upload_result
 
-    @image_app.route('', methods = ['POST'])
-    def upload_image():
+    @image_app.route('/seller', methods = ['POST'])
+    def upload_seller_image():
         image_service = ImageService()
-        image_upload_result = image_service.upload_image(request)
+        image_upload_result = image_service.upload_seller_image(request)
+        return image_upload_result
+
+    @image_app.route('/event', methods = ['POST'])
+    def upload_event_image():
+        image_service = ImageService()
+        image_upload_result = image_service.upload_event_image(request)
         return image_upload_result
